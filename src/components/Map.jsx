3148674fa8bcd084e07d49ca99c60ddcfe404731
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2023-06-12 19:51:08 -0400

    Fixed issue where map would not re-center after serarching for new location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
+import React from 'react'
 
 // The red (active) marker icon on the map
 const redMarker = L.icon({
@@ -17,9 +18,18 @@ const blueMarker = L.icon({
 })
 
 export default function Map(props) {
-    const mapCenter = [props.value[0].center.lat, props.value[0].center.lon]; // Sets center of the map to the coordinates of the given court
+    const [mapCenter, setMapCenter] = React.useState([0,0]); // Sets center of the map to the coordinates of the given court
+    const [key, setKey] = React.useState(0); // Key property to force map container update
     const zoomLevel = 12; // The zoom level of the map
 
+    // Effect to update the map's center state when the user searches for a new location
+    React.useEffect(() => {
+      if(props.value.length > 0){
+        setMapCenter([props.value[0].center.lat, props.value[0].center.lon]);
+        setKey(prevKey => prevKey + 1);
+      }
+    }, [props.value])
+    
     // Updates the index when clicking into another place
     function changeIndex(id){
         let newIndex = 0
@@ -36,7 +46,7 @@ export default function Map(props) {
     }
   
     return (
-      <MapContainer center={mapCenter} zoom={zoomLevel} className="map">
+      <MapContainer key={key} center={mapCenter} zoom={zoomLevel} className="map">
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {props.value.map(pin => (
           <Marker key={pin.id} position={[pin.center.lat, pin.center.lon]} icon={pin.id == props.index ? redMarker : blueMarker} eventHandlers={{click: () => changeIndex(pin.id)}}>
