f1b77a1bc9b6aa4d1322c4ffa01154eaa59a438c
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2023-06-22 21:31:32 -0400

    Updating build

---
+++ src/config.jsx
@@ -0,0 +1,8 @@
+export const firebaseValues = {
+    apiKey: "AIzaSyD6qtBhLAnOopFhoBkERnZ3R7TN_I2hj1g",
+    authDomain: "basketball-court-finder-6dc25.firebaseapp.com",
+    projectId: "basketball-court-finder-6dc25",
+    storageBucket: "basketball-court-finder-6dc25.appspot.com",
+    messagingSenderId: "1062044207640",
+    appId: "1:1062044207640:web:368d26c0174397a393916d"
+}
