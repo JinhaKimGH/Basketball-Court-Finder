bf18135c7128ec5008860d452b062104c1058beb
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2023-06-22 20:33:39 -0400

    Hid firebase database information

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
+import {firebaseValues} from "./config.jsx"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6qtBhLAnOopFhoBkERnZ3R7TN_I2hj1g",
-  authDomain: "basketball-court-finder-6dc25.firebaseapp.com",
-  projectId: "basketball-court-finder-6dc25",
-  storageBucket: "basketball-court-finder-6dc25.appspot.com",
-  messagingSenderId: "1062044207640",
-  appId: "1:1062044207640:web:368d26c0174397a393916d"
+  apiKey: firebaseValues.apiKey,
+  authDomain: firebaseValues.authDomain,
+  projectId: firebaseValues.projectId,
+  storageBucket: firebaseValues.storageBucket,
+  messagingSenderId: firebaseValues.messagingSenderId,
+  appId: firebaseValues.appId
 };
 
 const app = initializeApp(firebaseConfig);
