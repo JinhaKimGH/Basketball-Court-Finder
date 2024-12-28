6d24d71548a3889dd5b5ca5c9411bd60b5662f24
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2024-12-27 21:06:25 -0500

    New backend initialization

---
+++ backend/src/main/java/com/basketballcourtfinder/service/BasketballCourtFinderApplication.java
@@ -0,0 +1,13 @@
+package com.basketballcourtfinder.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballCourtFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballCourtFinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/basketballcourtfinder/service/BasketballCourtFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketballcourtfinder.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballCourtFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
