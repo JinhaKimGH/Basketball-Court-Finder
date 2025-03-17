aaa21ef22c2e45a4b1ade211423a9a224eaad291
Jinha <jinhakim0704@gmail.com>
2025-03-17 00:40:36 -0400

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Basketball-Court-Finder/',
   plugins: [react(), tsconfigPaths()],
 })
